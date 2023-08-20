71afb97fdcd79c35efcad1aa979158eefafb8c8a
hibah11 <hboba-2012@hotmail.com>
2023-08-21 02:55:46 +0300

    hw

---
+++ main.js
@@ -0,0 +1,132 @@
+const react1 = document.getElementById("root");
+
+let element = <>
+<div class="container-fluid pt-3 px-5 py-5 pb-0">
+        <p class="fs-1 text-center fw-medium text-primary">Hibah Yousef Alhussain</p>
+        <p class="text-center text-secondary fs-5">Management Information System Fresh Graduate, CTNS Certified</p>
+        <p class="text-black">I am an enthusiastic graduate in Management Information Systems (MIS) actively seeking a stimulating role within the information technology field. I aim to apply my technical expertise, problem-solving capabilities, and solid business acumen to make meaningful contributions to the success of an organization.</p>
+    </div>
+    <div class="container-fluid d-flex flex-row justify-content-around align-items-center border-top border-bottom p-3 border-black">
+        <div class="d-flex flex-row">
+            <span class="material-symbols-outlined">
+                mail
+                </span>
+        <p>HibahAlhussain@hotmail.com</p>
+        </div>
+        <div class="d-flex flex-row">
+            <span class="material-symbols-outlined">
+                phone_iphone
+                </span>
+        <p>+966506283952</p>
+        </div>
+        <div class="d-flex flex-row">
+            <span class="material-symbols-outlined">
+                location_on
+                </span>
+        <p>Riyadh</p>
+        </div>
+        <div class="d-flex flex-row">
+            <span class="material-symbols-outlined">
+                link
+                </span>
+        <a href="https://www.linkedin.com/in/hibahalhussain/">www.linkedin.com/in/hibahalhussain</a>
+        </div>
+    </div>
+    <div class="container-fluid d-flex flex-row p-4 justify-content-around">
+        <div class="flex-nowrap">
+                <p class="fw-bolder fs-4 text-primary">WORK EXPERIENCE</p>
+                <p class="fw-bold fs-5 text-decoration-underline">COOP trainee</p>
+                <p class="fw-light fs-5">Alahsa Municipality</p>
+                <p class="fst-italic fs-6 text-body-tertiary">03/2023-06/2023</p>
+                <p class="fst-italic fs-6 text-body-tertiary">Achievements/Tasks:</p>
+                <ul>
+                    <li>Have trained in both Human Resources and administrative affairs departments.</li>
+                    <li>Created an application using Excel Macro for the Department of Customer Service.</li>
+                    <li>Created an interactive chatbot for the Department of Administrative and financial affairs.</li>
+                </ul>
+                <p class="fw-bold fs-5 text-decoration-underline">JavaScript frontend Bootcamp for Web development trainee</p>
+                <p class="fw-light fs-5">Tuwaiq Academy</p>
+                <p class="fst-italic fs-6 text-body-tertiary">07/2023-09/2023</p>
+                <p class="fst-italic fs-6 text-body-tertiary">Achievements/Tasks:</p>
+                <ul>
+                    <li>HTML, CSS, and JavaScript projects and assessments plus submitting day-to-day challenges.</li>
+                </ul>
+                <p class="fw-bolder fs-4 text-primary">CERTIFICATES AND COURSES</p>
+                <ul class="list-group">
+                    <li class="text-decoration-underline list-group-item">Oracle Certified Associate training course</li>
+                    <p class="fst-italic fs-6 text-body-tertiary">02/2022</p>
+                    <li class="text-decoration-underline list-group-item">Certified Telecommunications Network Specialist (CTNS)</li>
+                    <p class="fst-italic fs-6 text-body-tertiary">02/2022-04/2022</p>
+                    <li class="text-decoration-underline list-group-item">Python Programming 101,102 (Sater Academy)</li>
+                    <p class="fst-italic fs-6 text-body-tertiary">11/2022</p>
+                    <li class="text-decoration-underline list-group-item">Bootstap (Sater Academy)</li>
+                    <p class="fst-italic fs-6 text-body-tertiary">11/2022</p>
+                    <li class="text-decoration-underline list-group-item">HTML, CSS Course (Sater Academy)</li>
+                    <p class="fst-italic fs-6 text-body-tertiary">11/2022</p>
+                </ul>
+                <p class="fw-bolder fs-4 text-primary">EDUCATION</p>
+                <p class="fw-bold fs-5 text-decoration-underline">Bachelor's Degree in Management Information Systems</p>
+                <p class="fw-light fs-5">King Faisal University</p>
+                <p class="fst-italic fs-6 text-body-tertiary">09/2018-06/2023</p>
+        </div>
+        <div class="flex-nowrap">
+            <p class="fw-bolder fs-4 text-primary">SKILLS</p>
+            <div class="d-flex flex-row justify-content-between">
+                <div class="d-flex flex-column">
+                    <p>HTML</p>
+                    <p>JavaScript</p>
+                    <p>Bootstrap</p>
+                    <p>GitHub</p>
+                    <p>System Analysis</p>
+                    <p>SDLC methodologies</p>
+                    <p>Excel Macro</p>
+                    <p>problems-solving</p>
+                </div>
+                <div class="d-flex flex-column">
+                    <p>CSS</p>
+                    <p>Python</p>
+                    <p>Git</p>
+                    <p>SQL databases</p>
+                    <p>ERP systems</p>
+                    <p>Teamwork</p>
+                    <p>Communication Skills</p>
+                    <p>Adaptability</p>
+                </div>
+            </div>
+            <p class="fw-bolder fs-4 text-primary">VOLUNTEERING</p>
+            <p class="fw-bold fs-5 text-decoration-underline">Member of MIS club</p>
+                <p class="fw-light fs-5">king Faisal University</p>
+                <p class="fst-italic fs-6 text-body-tertiary">01/2021-04/2021</p>
+                <p class="fst-italic fs-6 text-body-tertiary">Achievements/Tasks:</p>
+                <ul>
+                    <li>I have participated in my college (business college) weekly activities.</li>
+                </ul>
+                <p class="fw-bold fs-5 text-decoration-underline">Mentor</p>
+                <p class="fw-light fs-5">Afaq Almustaqbal team</p>
+                <p class="fst-italic fs-6 text-body-tertiary">06/2020</p>
+                <p class="fst-italic fs-6 text-body-tertiary">Achievements/Tasks:</p>
+                <ul>
+                    <li class="text-wrap">participated in the annual forum, presented my major and guided high school graduates</li>
+                </ul>
+                <p class="fw-bold fs-5 text-decoration-underline">Data entry</p>
+                <p class="fw-light fs-5">Ministry Of Health</p>
+                <p class="fst-italic fs-6 text-body-tertiary">06/2020-07/2020</p>
+                <p class="fst-italic fs-6 text-body-tertiary">Achievements/Tasks:</p>
+                <ul>
+                    <li class="text-wrap">A total of (134) volunteering hours, dealt with the Ministry of Health website to enter, update and delete COVID-19 daily cases (greater than 4k cases).</li>
+                </ul>
+                <p class="fw-bolder fs-4 text-primary">LANGUAGES</p>
+                <div class="d-flex flex-row justify-content-around">
+                    <div class="d-flex flex-column">
+                        <p>Arabic</p>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <p>English</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+</>
+
+
+ReactDOM.render(element, react1);
